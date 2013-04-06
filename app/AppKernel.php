77c776d4388c01d382868177b7cc54e8834d279a
Jorge Esquivel <eljeski@gmail.com>
2013-04-05 20:27:54 -0600

    Limpiando dependencias

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,10 +19,9 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-			new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new UNAH\SGOBundle\UNAHSGOBundle(),
-			new FOS\UserBundle\FOSUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
