0d181dee84d173de2b2eb357f246f6451b91e741
Jorge Esquivel <jbesquivel64@hotmail.com>
2013-08-29 21:15:39 -0600

    Agregar comando de migración al script de actualización

--- actualizar.sh
+++ actualizar.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+git stash save
 git pull
 
+php app/console doctrine:migrations:migrate
 php app/console assets:install
 php app/console cache:clear --env=prod
 chmod -R 777 .
