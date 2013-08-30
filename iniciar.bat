8c48587b7dc5c3731b189b84be7a767ca61b2903
Jorge Esquivel <jbesquivel64@hotmail.com>
2013-08-29 21:22:34 -0600

    Actualizar el script de inicio en windows

--- iniciar.bat
+++ iniciar.bat
@@ -1,1 +1,9 @@
-php app/console doctrine:migrations:version 20130406055131 --add
+git stash save
+git pull
+php composer.phar selfupdate
+php composer.phar install
+php app/console doctrine:migrations:migrate
+php app/console doctrine:fixtures:load
+php app/console assets:install
+php app/console cache:clear --env=prod
+git stash apply
